2f389638dce2ab78b94880140ba0ebed5d8ded80
Noah Kiser <noah.kiser@gmail.com>
2024-06-22 08:56:21 -0500

    complete lesson 5

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: [
+        { id: 9001, size: "small" },
+        { id: 9002, size: "medium" },
+        { id: 9003, size: "large" },
+        { id: 9004, size: "extra large" },
+      ],
+    };
+  },
+});
